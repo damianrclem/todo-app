c09870a6f99376c22deda220a195f77b1964aeca
Damian Clem <damianrclem@gmail.com>
2022-08-15 10:19:11 -0700

    imported todo provider context to handle updating list items

--- components/TaskInput/TaskInput.jsx
+++ components/TaskInput/TaskInput.jsx
@@ -1,19 +1,42 @@
+import { useState, useContext } from 'react';
+import { TodoContext } from '../../Providers/TodoProvider';
+
 import { AiOutlineAlignLeft, AiOutlinePlus } from 'react-icons/ai';
 
 import styles from './TaskInput.module.css';
 
-const TaskInput = () => {
+const TaskInput = ({ onChangeHandler }) => {
+    const [task, setTask] = useState();
+    const [tasks, setTasks] = useContext(TodoContext);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const newTasks = [
+            {
+                id: Math.round(Math.random() * 10000),
+                title: task,
+                isCompleted: false,
+            },
+            ...tasks,
+        ];
+
+        setTasks(newTasks);
+    };
+
     return (
-        <section className={styles.wrapper}>
-            <div className={styles.input__wrapper}>
-                <span>
-                    <AiOutlineAlignLeft className={styles.input__icon} />
-                </span>
-                <input type='text' className={styles.task__input} />
-                <button>
-                    <AiOutlinePlus />
-                </button>
-            </div>
+        <section className={styles.form_list_wrapper}>
+            <form onSubmit={onSubmit}>
+                <div className={styles.input__wrapper}>
+                    <span>
+                        <AiOutlineAlignLeft className={styles.input__icon} />
+                    </span>
+                    <input type='text' className={styles.task__input} onChange={onChangeHandler} />
+                    <button>
+                        <AiOutlinePlus />
+                    </button>
+                </div>
+            </form>
         </section>
     );
 };
