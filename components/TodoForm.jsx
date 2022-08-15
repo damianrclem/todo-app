6f5a58329aada4bd4dc6f9e2023b4b1b458ccbe2
Damian Clem <damianrclem@gmail.com>
2022-08-15 10:17:32 -0700

    Updated todo form to include context provider

--- components/TodoForm.jsx
+++ components/TodoForm.jsx
@@ -1,18 +1,27 @@
+import { useState, useContext } from 'react';
+
 import FormHeader from './Header/FormHeader';
 import CalendarScroll from './CalendarScroll/CalendarScroll';
 import TaskInput from './TaskInput/TaskInput';
+import TodoList from './TodoList/TodoList';
+
+import { TodoProvider } from '../Providers/TodoProvider';
 
 import styles from './TodoForm.module.css';
 
 const TodoForm = () => {
+    const onChangeHandler = (event) => {
+        setTask(event.target.value);
+    };
+
     return (
         <div className={styles.wrapper}>
-            {<FormHeader />}
-            {<CalendarScroll />}
-            <div className={styles.form_list_wrapper}>
-                {<TaskInput />}
-                {/* Input Todo List */}
-            </div>
+            <TodoProvider>
+                <FormHeader />
+                <CalendarScroll />
+                <TaskInput />
+                <TodoList />
+            </TodoProvider>
         </div>
     );
 };
